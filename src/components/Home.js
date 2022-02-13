87782d1491c8a519d5356afe53e2cd3ea2105266
Aman Chauhan <amangotpro@gmail.com>
2022-02-13 19:10:23 +0530

    Complete: Home components

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,56 @@ import Section from "./Section"
 
 function Home() {
     return (
-        <div>
-            <Section />
-        </div>
+        <Container>
+            <Section
+                title="Model S"
+                description="Order online for touchless delivery"
+                backgroundImg="modesl-s.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing inventory"
+            />
+            <Section
+                title="Model Y"
+                description="Order online for touchless delivery"
+                backgroundImg="modesl-y.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing inventory"
+            />
+            <Section
+                title="Model 3"
+                description="Order online for touchless delivery"
+                backgroundImg="modesl-3.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing inventory"
+            />
+            <Section
+                title="Model X"
+                description="Order online for touchless delivery"
+                backgroundImg="modesl-x.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing inventory"
+            />
+            <Section
+                title="Lowest Cost Solar Panels in America"
+                description="Money-back guarantee"
+                backgroundImg="solar-panel.jpg"
+                leftBtnText="Order now"
+                rightBtnText="Learn More"
+            />
+            <Section
+                title="Solar for new roof"
+                description="Solar Roof Costs Less Than a New Roof Plus Solar Panels"
+                backgroundImg="solar-panel.jpg"
+                leftBtnText="Order now"
+                rightBtnText="Learn More"
+            />
+            <Section
+                title="Accessories"
+                description=""
+                backgroundImg="accessories.jpg"
+                leftBtnText="Shop now"
+            />
+        </Container>
     )
 }
 
