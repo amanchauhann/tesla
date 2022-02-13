71ee95ed2dca644f4e6fe133cfa0b6aec8eb13f9
Aman Chauhan <amangotpro@gmail.com>
2022-02-13 21:30:59 +0530

    add: burgerNav functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
 import styled from "styled-components";
 import MenuIcon from '@mui/icons-material/Menu';
 import Fade from 'react-reveal/Fade';
+import CloseIcon from '@mui/icons-material/Close';
+import { useState } from "react";
 
 function Header() {
+    const [burgerStatus, setBurgerStatus] = useState(false);
+
+
     return (
         <Container>
             <a>
@@ -17,8 +22,24 @@ function Header() {
             <RightMenu>
                 <a href="#">Shop</a>
                 <a href="#">Tesla Account</a>
-                <CustomMenu />
+                <CustomMenu onClick={()=>setBurgerStatus(true)} />
             </RightMenu>
+            <BurgerNav show={burgerStatus}>
+                <CloseWrapper>
+                    <CustomClose onClick={()=>setBurgerStatus(false)} />
+                </CloseWrapper>
+                
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Used Inventory</a></li>
+                <li><a href="#">Trade-in</a></li>
+                <li><a href="#">CyberTruck</a></li>
+                <li><a href="#">Roadster</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+            </BurgerNav>
         </Container>
     )
 }
@@ -64,4 +85,34 @@ const RightMenu= styled.div`
 `
 const CustomMenu= styled(MenuIcon)`
     cursor: pointer;
+`
+const BurgerNav= styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background: white;
+    width: 300px;
+    z-index: 16;
+    list-style: none;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+    transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
+    li{
+        padding: 15px 0;
+        border-bottom: 1px solid rgba(0, 0, 0, .2);
+
+        a{
+            font-weight: 600;
+        }
+    }
+`
+const CustomClose= styled(CloseIcon)`
+    cursor: pointer;
+`
+const CloseWrapper= styled.div`
+    display: flex;
+    justify-content: flex-end;
 `
